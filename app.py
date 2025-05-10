afc22c1df19141ad192e4a8971e7035622146bc4
jacksonjabir <jacksonjabir@gmail.com>
2025-05-10 12:16:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,3 +28,8 @@ def download_audio():
 
     except subprocess.CalledProcessError as e:
         return jsonify({"error": "Download failed", "details": str(e)}), 500
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
